2f1ee4019c4c1ec4449a6ad64defbf3cbf0819a5
Jay.Ying <yingjie23@gmail.com>
2016-07-10 17:36:28 +0800

    add markers cluster

--- js/draw.js
+++ js/draw.js
@@ -9,39 +9,55 @@ var map = L.mapbox.map('map')
     .setView([31.21, 121.435], 16);
 
 // use openstreet tile layer
-
-//L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
-//    .addTo(map)
-//    .on('load', finishedLoading);
+/*
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
+    .addTo(map)
+    .on('load', finishedLoading);
+*/
 
 // use mapbox.streets
-
 L.mapbox.tileLayer('mapbox.streets')
     .addTo(map)
     .on('load', finishedLoading);
 
-
-var polyline = L.polyline([]).addTo(map);
+// Todo: support load paths
+var path = L.polyline([]).addTo(map);
 
 function finishedLoading() {
     // avoid redraw ployline when zooming map
-    var latlngs = polyline.getLatLngs();
+    var latlngs = path.getLatLngs();
     if (latlngs.length == 0) {
         var processData = function (error, results) {
-            var features = results[0].features;
-            var coordinates =  features[0].geometry.coordinates;
+            // Start drawing the path.
+            var paths = results[0].features;
+            var coordinates = paths[0].geometry.coordinates;
             var points = 0
-            // Start drawing the polyline.
             add();
             function add() {
                 var entry = coordinates[points]
-                polyline.addLatLng(L.latLng(entry[1], entry[0]));
+                path.addLatLng(L.latLng(entry[1], entry[0]));
                 if (++points < coordinates.length) window.setTimeout(add, 300);
             }
+
+            // start drawing the markers
+            var spots = results[1].features;
+            var markers = new L.MarkerClusterGroup();
+
+            for (var i = 0; i < spots.length; i++) {
+                var spot = spots[i];
+                var latlng = spot.geometry.coordinates;
+                var marker = L.marker(new L.LatLng(latlng[1], latlng[0]), {
+                    icon: L.mapbox.marker.icon(spot.properties),
+                    title: spot.properties.title
+                });
+                marker.bindPopup(spot.properties.description);
+                markers.addLayer(marker);
+            }
+            map.addLayer(markers);
         };
 
         (function fetchData() {
-            var dataFetchers = _(['features.geojson']).map(function (fileName) {
+            var dataFetchers = _(['paths.json', 'markers.json']).map(function (fileName) {
                 return function (callback) {
                 $.getJSON('data/' + fileName, function (data) {
                     callback(null, data);
@@ -53,11 +69,12 @@ function finishedLoading() {
 
         var coordinates = document.getElementById('coordinates');
         var marker = L.marker([31.210947924347657,121.43598318099974], {
-            icon: L.mapbox.marker.icon({
-            'marker-color': '#f86767'
-            }),
+            icon: L.mapbox.marker.icon({'marker-color': '#f86767'}),
+            title: "locator",
             draggable: true
-        }).addTo(map);
+        });
+        marker.bindPopup("定位器，用来查找坐标");
+        marker.addTo(map);
 
         // every time the marker is dragged, update the coordinates container
         marker.on('dragend', ondragend);
