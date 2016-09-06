bfb166f837a7fdd4eaa9e19779d56fc4b679f5d9
Jay.Ying <yingjie23@gmail.com>
2016-09-06 21:51:08 +0800

    refactor the loading process & data file structure

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
 # walk-in-shanghai
 An interactive map for storing the moments
+
+## GeoJson File Format
+
+one geojson file represents a scenery, which composed by the following parts:
+
+### Site
+
+The site feature layer is an marker for creating nav bar and locating to a specific scenery.
+
+    {
+      "type": "Site",
+      "properties": {
+        "title": "武康路",
+        "marker-size": "medium",
+        "marker-color": "#e7857f",
+        "marker-symbol": "heart"
+      },
+      "geometry": {
+        "coordinates": [
+          121.435,
+          31.21
+        ],
+        "type": "Point"
+      }
+    }
+
+### Sign
+
+a "Sign" marker will be placed on the map without the effect of zooming. It could be used as the start-stop operator.
+
+    {
+      "type": "Sign",
+      "properties": {
+        "description": "start",
+        "marker-size": "medium",
+        "marker-color": "#e7857f",
+        "marker-symbol": "circle"
+      },
+      "geometry": {
+        "coordinates": [
+          121.433330,
+          31.205900
+        ],
+        "type": "Point"
+      }
+    }
+
+### Path
+
+If the scenery is a road, the "Path" object will be drawed during initial loading.
+
+    {
+       "type":"Path",
+       "properties":{
+          "title":"武康路",
+          "description":"武康路全长1700米",
+          "stroke":"#1087bf",
+          "stroke-width":4,
+          "stroke-opacity":1
+       },
+       "geometry":{
+          "coordinates":[
+             [
+                121.4333,
+                31.205942
+             ],
+             [
+                121.433354,
+                31.206134
+             ],
+             [
+                121.433499,
+                31.206364
+             ]
+          ],
+          "type":"LineString"
+       }
+    }
+
+### Spot
+
+The "Spot" points will form a cluster group when zooming out.
+
+
+    {
+      "type": "Spot",
+      "properties": {
+        "id": "marker-ip212jtx0",
+        "title": "grains",
+        "description": "coffee ",
+        "marker-size": "medium",
+        "marker-color": "#e7857f",
+        "marker-symbol": "cafe",
+        "images": [
+            [
+                "http://youimg1.c-ctrip.com/target/fd/tg/g1/M08/82/16/CghzfFWX3aOAC8vfABIa8JcbLno411.jpg",
+                "武康路是一条安静低调，充满历史文化气息的马路."
+            ],
+            [
+                "http://file29.mafengwo.net/M00/4B/E0/wKgBpVVivPCAHIxAABAX35qfKF473.jpeg",
+                "穿越上海百年的时光隧道."
+            ],
+            [
+                "http://file29.mafengwo.net/M00/4B/FA/wKgBpVVivQuAPBzfABGCCTUcw-E35.jpeg",
+                "高大的梧桐树，炎炎夏日绿色的遮盖."
+            ]
+         ]
+      },
+      "geometry": {
+        "coordinates": [
+          121.435275,
+          31.21137
+        ],
+        "type": "Point"
+      }
+    }
+
--- js/draw.js
+++ js/draw.js
@@ -1,35 +1,70 @@
-// Todo:
+/*
+ Todo:
+ https://www.mapbox.com/mapbox.js/example/v1.0.0/scroll-driven-navigation/
+ https://www.mapbox.com/mapbox.js/example/v1.0.0/reorder-listing-based-on-marker-proximity/
+ https://www.mapbox.com/mapbox.js/example/v1.0.0/marker-tooltip-tab-groups/
+ mustache template
+*/
 
-// add a trigger to draw path
-// geojson file structure, set a filter of different marker
+/*
+ Reference
+
+ maki icons
+ https://www.mapbox.com/maki-icons/
+
+ map events
+ http://leafletjs.com/reference-1.0.0.html#map-event
+
+ geojson
+ http://geojson.org/geojson-spec.html
+
+ async js
+ https://github.com/caolan/async/blob/v1.5.2/README.md
+
+ underscore js
+ http://underscorejs.org/#collections
+
+ mustache
+ https://github.com/janl/mustache.js
+
+*/
+
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoibm9vbXJldmxpcyIsImEiOiJjaW91Nm1hem8wMG83dW9tOHoycTdtbnRsIn0.ubjVj1rxUJ9rDVv_xppzHA';
 var map = L.mapbox.map('map')
     .setView([31.21, 121.435], 16);
 
-// use openstreet tile layer
 /*
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
     .addTo(map)
     .on('load', finishedLoading);
 */
 
-// use mapbox.streets
 L.mapbox.tileLayer('mapbox.streets')
     .addTo(map)
     .on('load', finishedLoading);
 
-// Todo: support load paths
-var path = L.polyline([]).addTo(map);
+var markerList = document.getElementById('marker-list');
+var isGeoJsonLoaded = false;
 
-function finishedLoading() {
-    // avoid redraw ployline when zooming map
-    var latlngs = path.getLatLngs();
-    if (latlngs.length == 0) {
-        var processData = function (error, results) {
-            // Start drawing the path.
-            var paths = results[0].features;
-            var coordinates = paths[0].geometry.coordinates;
+function generateNavBar(site, pathData) {
+    var item = markerList.appendChild(document.createElement('li'));
+    item.innerHTML = site.properties.title;
+    var latlng = site.geometry.coordinates;
+    var pos = new L.LatLng(latlng[1], latlng[0]);
+    var marker = L.marker(pos, {
+        icon: L.mapbox.marker.icon(site.properties),
+        title: site.properties.title
+    });
+    marker.bindPopup(site.properties.title);
+    var layer = marker.addTo(map);
+    var path = L.polyline([]).addTo(map);
+    item.onclick = function() {
+        map.setView(pos, 16);
+        var latlngs = path.getLatLngs()
+        // avoid repeat draw path
+        if (latlngs.length == 0) {
+            var coordinates = pathData.geometry.coordinates;
             var points = 0
             add();
             function add() {
@@ -37,62 +72,101 @@ function finishedLoading() {
                 path.addLatLng(L.latLng(entry[1], entry[0]));
                 if (++points < coordinates.length) window.setTimeout(add, 300);
             }
+        }
+        layer.openPopup();
+    };
+};
 
-            // start drawing the markers
-            var spots = results[1].features;
-            var markers = new L.MarkerClusterGroup();
+function generateSpotCluster(spots) {
+    var markers = new L.MarkerClusterGroup();
+    for (var i = 0; i < spots.length; i++) {
+        var spot = spots[i];
+        var latlng = spot.geometry.coordinates;
+        var marker = L.marker(new L.LatLng(latlng[1], latlng[0]), {
+            icon: L.mapbox.marker.icon(spot.properties),
+            title: spot.properties.title
+        });
 
-            for (var i = 0; i < spots.length; i++) {
-                var spot = spots[i];
-                var latlng = spot.geometry.coordinates;
-                var marker = L.marker(new L.LatLng(latlng[1], latlng[0]), {
-                    icon: L.mapbox.marker.icon(spot.properties),
-                    title: spot.properties.title
-                });
+        var images = spot.properties.images;
+        var slideshowContent = '';
+        for(var j = 0; j < images.length; j++) {
+            var img = images[j];
 
-                var images = spot.properties.images;
-                var slideshowContent = '';
-                for(var j = 0; j < images.length; j++) {
-                    var img = images[j];
+            slideshowContent += '<div class="image' + (j === 0 ? ' active' : '') + '">' +
+                                    '<img src="' + img[0] + '" />' +
+                                    '<div class="caption">' + img[1] + '</div>' +
+                                '</div>';
+        }
 
-                    slideshowContent += '<div class="image' + (j === 0 ? ' active' : '') + '">' +
-                                          '<img src="' + img[0] + '" />' +
-                                          '<div class="caption">' + img[1] + '</div>' +
-                                        '</div>';
-                }
+        // Create custom popup content
+        var popupContent = '<div id="' + spot.properties.id + '" class="popup">' +
+                                '<h2>' + spot.properties.title + '</h2>' +
+                                '<div class="slideshow">' +
+                                    slideshowContent +
+                                '</div>' +
+                                '<div class="cycle">' +
+                                    '<a href="#" class="prev">&laquo; Previous</a>' +
+                                    '<a href="#" class="next">Next &raquo;</a>' +
+                                '</div>'
+                            '</div>';
+        marker.bindPopup(popupContent, {
+            closeButton: true,
+            minWidth: 320
+        });
+        markers.addLayer(marker);
+        /*
+        markers.on('contextmenu', function(e) {
+            e.layer.openPopup();
+        });
+        markers.on('click', function(e) {
+            e.layer.closePopup();
+        });
+        */
+    }
+    map.addLayer(markers);
+};
 
-                // Create custom popup content
-                var popupContent = '<div id="' + spot.properties.id + '" class="popup">' +
-                                       '<h2>' + spot.properties.title + '</h2>' +
-                                       '<div class="slideshow">' +
-                                           slideshowContent +
-                                       '</div>' +
-                                       '<div class="cycle">' +
-                                           '<a href="#" class="prev">&laquo; Previous</a>' +
-                                           '<a href="#" class="next">Next &raquo;</a>' +
-                                       '</div>'
-                                   '</div>';
-                marker.bindPopup(popupContent, {
-                    closeButton: false,
-                    minWidth: 320
-                });
-                markers.addLayer(marker);
-                markers.on('mouseover', function(e) {
-                    e.layer.openPopup();
-                });
-                markers.on('mouseout', function(e) {
-                    e.layer.closePopup();
-                });
+
+function processData(error, results) {
+    for (var i = 0; i < results.length; i++) {
+        var features = results[i].features;
+        var spots = [];
+        var site, pathData;
+        for (var j = 0; j<features.length; j++) {
+            var feature = features[j];
+            if (feature.type == "Site") {
+                site = feature;
             }
-            map.addLayer(markers);
-        };
-
-        (function fetchData() {
-            var dataFetchers = _(['paths.json', 'markers.json']).map(function (fileName) {
-                return function (callback) {
-                $.getJSON('data/' + fileName, function (data) {
-                    callback(null, data);
+            if (feature.type == "Spot") {
+                spots.push(feature)
+            }
+            if (feature.type == "Sign") {
+                var latlng = feature.geometry.coordinates;
+                var marker = L.marker(new L.LatLng(latlng[1], latlng[0]), {
+                    icon: L.mapbox.marker.icon(feature.properties),
+                    title: feature.properties.title
                 });
+                //marker.bindPopup("content");
+                marker.addTo(map);
+            }
+            if (feature.type == "Path") {
+                pathData = feature;
+            }
+        }
+        generateNavBar(site, pathData);
+        generateSpotCluster(spots);
+    }
+};
+
+function finishedLoading() {
+    if (!isGeoJsonLoaded) {
+        (function fetchData() {
+            // Todo change getfiles in server side
+            var dataFetchers = _.map(['wukangroad.json'], function (fileName) {
+                return function (callback) {
+                    $.getJSON('data/' + fileName, function (data) {
+                        callback(null, data);
+                    });
                 };
             });
             async.parallel(dataFetchers, processData);
@@ -115,8 +189,9 @@ function finishedLoading() {
             var m = marker.getLatLng();
             coordinates.innerHTML = 'Latitude: ' + m.lat + '<br />Longitude: ' + m.lng;
         }
+        isGeoJsonLoaded = true;
     }
-}
+};
 
 $('#map').on('click', '.popup .cycle a', function() {
     var $slideshow = $('.slideshow'),
@@ -138,6 +213,3 @@ $('#map').on('click', '.popup .cycle a', function() {
     $newSlide.addClass('active').show();
     return false;
 });
-     
-            
-
